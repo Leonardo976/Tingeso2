9ddb67dc3759334b4b96283c2bae49f82b412ca0
Leonardo <leonardo.munoz.f@usach.cl>
2023-10-30 18:57:54 -0300

    [Leonardo Munoz][Create] studentsService, junto a el inicio de eureka y dockerfile (todo adjunto a cambios)

---
+++ student-service/src/main/java/com/example/studentservice/controllers/StudentController.java
@@ -0,0 +1,85 @@
+package com.example.studentservice.controllers;
+
+import com.example.studentservice.entities.StudentEntity;
+import com.example.studentservice.models.FeeModel;
+import com.example.studentservice.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<StudentEntity>> getStudents(){
+        List<StudentEntity> students = studentService.getStudents();
+        if(students.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{rut}")
+    public ResponseEntity<StudentEntity> findByRut(@PathVariable("rut") String rut){
+        StudentEntity student = studentService.findByRut(rut);
+        if(student == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PostMapping("/new-student")
+    public void saveStudent(@RequestBody StudentEntity student){
+        studentService.saveStudent(student);
+    }
+
+    @DeleteMapping("/delete-all")
+    public void deleteStudents(){
+        studentService.deleteStudents();
+    }
+
+    @GetMapping("/fees/{rut}")
+    public ResponseEntity<List<FeeModel>> getFees(@PathVariable("rut") String rut){
+        StudentEntity student = studentService.findByRut(rut);
+        if(student == null){
+            return ResponseEntity.notFound().build();
+        }
+        List<FeeModel> fees = studentService.getFees(rut);
+        if(fees.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(fees);
+    }
+
+    @PostMapping("/set-max-number-of-fees/{rut}/{number_of_fees}")
+    public void setMaxNumberOfFees(@PathVariable("rut") String rut, @PathVariable("number_of_fees") Integer number_of_fees){
+        studentService.setPaymentMethod(rut, number_of_fees);
+    }
+
+    @GetMapping("/calculate-discount-senior-year/{rut}")
+    public ResponseEntity<Double> calculateDiscountBySeniorYear(@PathVariable("rut") String rut){
+        StudentEntity student = studentService.findByRut(rut);
+        if(student == null){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(studentService.calculateDiscountBySeniorYear(rut));
+    }
+
+    @GetMapping("/calculate-discount-school-type/{rut}")
+    public ResponseEntity<Double> calculateDiscountBySchoolType(@PathVariable("rut") String rut){
+        StudentEntity student = studentService.findByRut(rut);
+        if(student == null){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(studentService.calculateDiscountBySchoolType(rut));
+    }
+
+}
---
+++ student-service/src/main/java/com/example/studentservice/services/StudentService.java
@@ -0,0 +1,143 @@
+package com.example.studentservice.services;
+
+import com.example.studentservice.entities.StudentEntity;
+import com.example.studentservice.models.FeeModel;
+import com.example.studentservice.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<StudentEntity> getStudents(){
+        return studentRepository.findAll();
+    }
+
+    public String getSchoolType(String rut){
+        return studentRepository.findSchoolType(rut);
+    }
+
+    public Integer getSeniorYear(String rut){
+        return studentRepository.findSeniorYear(rut);
+    }
+
+    public String getPaymentMethod(String rut){
+        return studentRepository.findPaymentMethod(rut);
+    }
+
+    public StudentEntity findByRut(String rut){
+        return studentRepository.findByRut(rut);
+    }
+
+    public void saveStudent(StudentEntity student){
+        studentRepository.save(student);
+    }
+
+    public void deleteStudents(){
+        studentRepository.deleteAll();
+    }
+
+    public Double calculateDiscountBySeniorYear(String rut){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String senior_year = getSeniorYear(rut).toString();
+        LocalDate dBefore = LocalDate.parse("31/12/" + senior_year, formatter);
+        LocalDate dAfter = LocalDate.now();
+
+        long diff = dBefore.until(dAfter, ChronoUnit.YEARS);
+        double total_discount = 0.0;
+
+        if(diff < 1){
+            total_discount = 1500000 * 0.15;
+        }else if(diff <= 2){
+            total_discount = 1500000 * 0.08;
+        }else if(diff <= 4){
+            total_discount = 1500000 * 0.04;
+        }
+
+        return total_discount;
+    }
+
+    public Double calculateDiscountBySchoolType(String rut){
+        double total_discount = 0.0;
+        String school_type = getSchoolType(rut);
+
+        if(school_type.equals("Municipal")){
+            total_discount = 1500000 * 0.2;
+        }else if(school_type.equals("Subvencionado")){
+            total_discount = 1500000 * 0.1;
+        }
+        return total_discount;
+    }
+
+    public Double calculateFinalPriceByDiscounts(String rut){
+        double total = 1500000.0;
+        if(getPaymentMethod(rut).equals("Cuotas")){
+            double discount_school_type = calculateDiscountBySchoolType(rut);
+            double discount_senior_year = calculateDiscountBySeniorYear(rut);
+            return total - discount_senior_year - discount_school_type;
+        }else{
+            return total;
+        }
+    }
+
+    public List<FeeModel> getFees(String rut){
+        return restTemplate.getForObject("http://fee-service/fees/by-student/"+rut, List.class);
+    }
+
+    public void setMaxNumberOfFees(String rut, Integer number_of_fees){
+        StudentEntity student = findByRut(rut);
+        String school_type = getSchoolType(rut);
+        if(number_of_fees != 0){
+            if(school_type.equals("Municipal")){
+                student.setNumber_of_fees(Math.min(number_of_fees, 10));
+            }else if(school_type.equals("Subvencionado")){
+                student.setNumber_of_fees(Math.min(number_of_fees, 7));
+            }else{
+                student.setNumber_of_fees(Math.min(number_of_fees, 4));
+            }
+        }else{
+            student.setPayment_method("Contado");
+        }
+
+        saveStudent(student);
+
+    }
+
+    public void setFinalPriceByPaymentMethod(String rut){
+        StudentEntity student = findByRut(rut);
+        String payment_method = getPaymentMethod(rut);
+
+        if(payment_method.equals("Contado")){
+            student.setFinal_price(1500000*0.5);
+        }
+        saveStudent(student);
+    }
+
+    public void setPaymentMethod(String rut, Integer number_of_fees){
+        StudentEntity student = findByRut(rut);
+        if(number_of_fees == 0){
+            student.setPayment_method("Contado");
+        }else{
+            student.setPayment_method("Cuotas");
+        }
+
+        setFinalPriceByPaymentMethod(rut);
+        setMaxNumberOfFees(rut, number_of_fees);
+        Double final_price = calculateFinalPriceByDiscounts(rut);
+        student.setFinal_price(final_price);
+        saveStudent(student);
+    }
+
+}
+
