5dcb1c76a5b6601fd946c3b4738e6828dfdff7ef
Leonardo <leonardo.munoz.f@usach.cl>
2023-11-05 21:00:57 -0300

    [Leonardo Munoz][Feature] modificaciones de propierties.yml, donde se agregó conexion a la base de datos, se comenzará con la modificación de la vista

--- my-app2/src/components/FooterComponent.jsx
+++ my-app2/src/components/FooterComponent.jsx
@@ -4,8 +4,7 @@ import styled from 'styled-components';
 const FooterComponent = () => {
     return (
         <FooterContainer>
-            <p>Valentina Paz Campos Olguín</p>
-            <p>Técnicas de Ingeniería de Software</p>
+            <p>Tingeso</p>
             <p>2-2023</p>
         </FooterContainer>
     );
